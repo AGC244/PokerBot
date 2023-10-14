7f0426a79f64151c6c91af79c36e588f84649955
Aiden <AGCleary244@gmail.com>
2023-10-14 13:47:09 +0100

    first commit

--- Poker.py
+++ Poker.py
@@ -1,1 +1,14 @@
+'''
+Simple tool that lets you choose which hand has better odds at each stage of the game
+'''
+
 cards = []
+
+for i in range(52):
+    cards.append(i)
+
+hand1 = []
+hand2 =  []
+
+
+print(cards)
